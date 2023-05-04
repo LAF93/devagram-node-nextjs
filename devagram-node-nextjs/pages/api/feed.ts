fc94c475e9818793c090abb1d8b21f7d40757060
LUCAS AMARAL FAGUNDES <120145358+LAF93@users.noreply.github.com>
2023-05-03 21:37:04 -0300

    get do usuario e id do user

--- devagram-node-nextjs/pages/api/feed.ts
+++ devagram-node-nextjs/pages/api/feed.ts
@@ -2,36 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import { UsuarioModel } from "@/models/UsuarioModel";
-import { PublicacaoModel } from "@/models/PublicacaoModel";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
 
-const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
+const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
     try {
-        if(req.method === 'GET'){
-            // receber uma informação do id do usuario
-            // que eu quero buscar o feed
-            // onde vem essa informação?
-            if(req?.query?.id){
-                // agora que tenho o id do usuario
-                // como eu valido se o usuario valido
+        if (req.method === 'GET') {
+            if (req?.query?.id) {
                 const usuario = await UsuarioModel.findById(req?.query?.id);
-                if(!usuario){
-                    return res.status(400).json({erro : 'Usuario não encontrado'});
+
+                if (!usuario) {
+                    return res.status(400).json({ erro: 'Usuario não encontrado' });
                 }
+                const publicacoes = await PublicacaoModel
+                    .find({ idUsuario: usuario._id })
+                    .sort({ data: -1 });
 
-                // e como eu busco as publicações dele?
-                const publicações = await PublicacaoModel
-                .find({idUsuario : usuario._id})
-                .sort({data : -1});
-
-                return res.status(200).json(publicações)
+                return res.status(200).json(publicacoes)
             }
         }
-        return res.status(405).json({erro : 'Metodo Informado não e valido'});
+        return res.status(405).json({ erro: 'Metodo Informado não e valido' });
     } catch (e) {
         console.log(e);
     }
-    return res.status(400).json({erro : 'Não foi possivel obter feed'});
+    return res.status(400).json({ erro: 'Não foi possivel obter feed' });
 }
 
 export default validarTokenJWT(conectarMongoDB(feedEndpoint));
