39d4f950d9f74ea996f9674085ddc5b0fb4d523d
LUCAS AMARAL FAGUNDES <120145358+LAF93@users.noreply.github.com>
2023-05-07 10:42:13 -0300

    pesquisa de usurário concluída

--- devagram-node-nextjs/pages/api/pesquisa.ts
+++ devagram-node-nextjs/pages/api/pesquisa.ts
@@ -1,4 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { UsuarioModel } from "@/models/UsuarioModel";
 
-const pesquisaEndPoint = (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>)
+const pesquisaEndPoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+
+    try {
+        if (req.method === 'GET') {
+
+            const { filtro } = req.query;
+            if (!filtro || filtro.length < 2) {
+                return res.status(400).json({ erro: 'Favor informar pelo menos 2 caracteres para a buscar' });
+            }
+
+            const usuariosEncontrados = await UsuarioModel.find({
+                nome : {$regex : filtro, $options: 'i'}
+
+                // pesquisa por nome e email
+                // $or : [{nome : {$regex : filtro, $options: 'i'}},
+                //     {email : {$regex : filtro, $options: 'i'}}]
+            });
+
+            return res.status(200).json(usuariosEncontrados);
+
+        }
+        return res.status(405).json({ erro: 'Metodo informado nao e Valido' })
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ erro: 'Não foi possivel buscar usuario' })
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(pesquisaEndPoint));
