e2110f07f03bd42ed3964920dbde4ba5737c939e
LUCAS AMARAL FAGUNDES <120145358+LAF93@users.noreply.github.com>
2023-05-09 22:43:25 -0300

    correcção

--- devagram-node-nextjs/pages/api/cadastro.ts
+++ devagram-node-nextjs/pages/api/cadastro.ts
@@ -12,48 +12,48 @@ import nc from 'next-connect';
 const handler = nc()
     .use(updload.single('file'))
     .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-        try{
-        const usuario = req.body as CadastroRequisicao;
+        try {
+            const usuario = req.body as CadastroRequisicao;
 
-        if (!usuario.nome || usuario.nome.length < 3) {
-            return res.status(400).json({ erro: 'Nome invalido' });
+            if (!usuario.nome || usuario.nome.length < 3) {
+                return res.status(400).json({ erro: 'Nome invalido' });
+            }
+
+            if (!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes("@")
+                || !usuario.email.includes(".")) {
+                return res.status(400).json({ erro: 'Email invalido' });
+            }
+
+            const specialChars = /[!@#$%^&*(),.?":{}|<>]/;
+            if (!usuario.senha || usuario.senha.length < 8 || !specialChars.test(usuario.senha)) {
+                return res.status(400).json({ erro: 'Senha inválida. A senha deve conter pelo menos um caracter especial (!@#$%^&*(),.?":{}|<>)' });
+            }
+
+            // validacao se ja existe usuario com o mesmo email
+            const usuarioComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
+            if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+                return res.status(400).json({ erro: 'Email ja cadastrado!' })
+            }
+
+            //enviar a imagem do multer para cosmic
+            const image = await uploadImagemCosmic(req);
+
+            //Salva banco de dados
+            const usuarioASerSalvo = {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: md5(usuario.senha),
+                avatar: image?.media?.url,
+            }
+            await UsuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({ msg: 'Cadastro criado com sucesso' });
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ erro: 'Erro ao cadastrar usuario' })
         }
-        
-        if (!usuario.email || usuario.email.length < 5
-            || !usuario.email.includes("@")
-            || !usuario.email.includes(".")) {
-            return res.status(400).json({ erro: 'Email invalido' });
-        }
 
-        const specialChars = /[!@#$%^&*(),.?":{}|<>]/;
-        if(!usuario.senha || usuario.senha.length < 8 || !specialChars.test(usuario.senha)){
-            return res.status(400).json({erro: 'Senha inválida. A senha deve conter pelo menos um caracter especial (!@#$%^&*(),.?":{}|<>)'});
-          }
-
-        // validacao se ja existe usuario com o mesmo email
-        const usuarioComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
-        if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
-            return res.status(400).json({ erro: 'Email ja cadastrado!' })
-        }
-
-        //enviar a imagem do multer para cosmic
-        const image = await uploadImagemCosmic(req);
-
-        //Salva banco de dados
-        const usuarioASerSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha),
-            avatar: image?.media?.url,
-        }
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({ msg: 'Cadastro criado com sucesso' });
-    }catch(e){
-        console.log(e);
-        return res.status(500).json({erro : 'Erro ao cadastrar usuario'})
-    }
-
-});
+    });
 
 export const config = {
     api: {
