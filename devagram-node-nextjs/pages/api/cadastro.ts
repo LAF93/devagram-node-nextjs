354f154f34d63d63622eaa47fc6adb614252007f
LUCAS AMARAL FAGUNDES <120145358+LAF93@users.noreply.github.com>
2023-05-11 21:12:56 -0300

    correção next connect

--- devagram-node-nextjs/pages/api/cadastro.ts
+++ devagram-node-nextjs/pages/api/cadastro.ts
@@ -11,53 +11,51 @@ import nc from 'next-connect';
 
 const handler = nc()
     .use(updload.single('file'))
-    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-        try {
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        try{
             const usuario = req.body as CadastroRequisicao;
-
-            if (!usuario.nome || usuario.nome.length < 3) {
-                return res.status(400).json({ erro: 'Nome invalido' });
+        
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({erro : 'Nome invalido'});
             }
-
-            if (!usuario.email || usuario.email.length < 5
-                || !usuario.email.includes("@")
-                || !usuario.email.includes(".")) {
-                return res.status(400).json({ erro: 'Email invalido' });
+    
+            if(!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro : 'Email invalido'});
             }
-
-            const specialChars = /[!@#$%^&*(),.?":{}|<>]/;
-            if (!usuario.senha || usuario.senha.length < 8 || !specialChars.test(usuario.senha)) {
-                return res.status(400).json({ erro: 'Senha inválida. A senha deve conter pelo menos um caracter especial (!@#$%^&*(),.?":{}|<>)' });
+    
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({erro : 'Senha invalida'});
             }
-
+    
             // validacao se ja existe usuario com o mesmo email
-            const usuarioComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
-            if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
-                return res.status(400).json({ erro: 'Email ja cadastrado!' })
+            const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+                return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
             }
 
-            //enviar a imagem do multer para cosmic
+            // enviar a imagem do multer para o cosmic
             const image = await uploadImagemCosmic(req);
-
-            //Salva banco de dados
+    
+            // salvar no banco de dados
             const usuarioASerSalvo = {
-                nome: usuario.nome,
-                email: usuario.email,
-                senha: md5(usuario.senha),
-                avatar: image?.media?.url,
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : md5(usuario.senha),
+                avatar : image?.media?.url
             }
             await UsuarioModel.create(usuarioASerSalvo);
-            return res.status(200).json({ msg: 'Cadastro criado com sucesso' });
-        } catch (e) {
+            return res.status(200).json({msg : 'Usuario criado com sucesso'});
+        }catch(e : any){
             console.log(e);
-            return res.status(500).json({ erro: 'Erro ao cadastrar usuario' })
+            return res.status(400).json({erro : e.toString()});
         }
-
-    });
+});
 
 export const config = {
     api: {
-        bodyParser: false
+        bodyParser : false
     }
 }
 
